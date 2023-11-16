5734cda2d0eaa91f1d9637204aad1a245e05848c
NIKOLAY <stin541988@gmail.com>
2023-11-16 13:20:17 +0200

    Add implementation for GetService method

---
+++ CoachTimetableEditorApp/GUI/Pages/SpecifyPathToFile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoachTimetableEditorApp.GUI.Pages
+{
+    public class SpecifyPathToFileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
